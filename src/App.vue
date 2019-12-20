1cd39fb0d0a9313492bcd12babad6b86ca6b7d50
Todd Blaisdell <toddb@rocketcom.com>
2019-12-20 11:09:39 -0700

    Initial commit of Astro Vue Boilerplate

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+     <rux-global-status-bar appName="Astro Vue Bootstrap">
+      <rux-clock></rux-clock>
+    </rux-global-status-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Astro Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+@import '/src/assets/astro.core.min.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../node_modules/@astrouxds/rux-global-status-bar';
+import '../node_modules/@astrouxds/rux-clock';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
